fa1667763eb365f45e5ec7c092d9f3f2f42d1267
scarr023 <scarr023@student.wethinkcode.co.za>
2024-07-16 11:25:57 +0200

    catpuccin theme with backup

--- lua/plugins/themes/material.lua
+++ lua/plugins/themes/material.lua
@@ -1,7 +1,7 @@
 return {
     'marko-cerovac/material.nvim',
     config = function()
+        vim.g.material_style = "darker"
         vim.cmd 'colorscheme material'
-        vim.g.material_style = "palenight"
     end
 }
