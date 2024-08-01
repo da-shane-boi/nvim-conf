4a75a294ecc4268e2addb955613b366c8fa3eaf5
scarr023 <scarr023@student.wethinkcode.co.za>
2024-08-01 08:02:25 +0200

    zoxide

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,8 @@ return {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.5',
         dependencies = {
-            'nvim-lua/plenary.nvim'
+            'nvim-lua/plenary.nvim',
+            'jvgrootveld/telescope-zoxide',
         },
         config = function()
             local builtin = require("telescope.builtin")
@@ -16,11 +17,30 @@ return {
     {
         "nvim-telescope/telescope-ui-select.nvim",
         config = function()
+            local z_utils = require("telescope._extensions.zoxide.utils")
             require("telescope").setup {
                 extensions = {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown {
                         }
+                    },
+                    zoxide = {
+                        prompt_title = "[ Walking on the shoulders of TJ ]",
+                        mappings = {
+                            default = {
+                                after_action = function(selection)
+                                    print("Update to (" .. selection.z_score .. ") " .. selection.path)
+                                end
+                            },
+                            ["<C-u>"] = {
+                                before_action = function(selection) print("before C-u") end,
+                                keepinsert = true,
+                                action = function(selection)
+                                    vim.cmd.edit(selection.path)
+                                end
+                            },
+                            ["<C-q>"] = { action = z_utils.create_basic_command("split") },
+                        },
                     }
                 }
             }
