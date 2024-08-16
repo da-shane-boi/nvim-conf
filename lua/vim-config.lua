32a80017b837b147f3dfcdd0e51cb9704a71ef06
scarr023 <scarr023@student.wethinkcode.co.za>
2024-08-16 11:25:15 +0200

    diagnostics

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -23,11 +23,21 @@ vim.opt.autoread = true
 
 vim.opt.smartcase = true
 vim.opt.ignorecase = true
+--
+-- Clipboard configuration
+vim.opt.clipboard:append("unnamedplus")
+
+
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
 -- KEYS
 -- ease of use
 vim.keymap.set('n', '<C-x>', ':q!<CR>')
 vim.keymap.set('n', '<C-s>', ':w<CR>')
+
 
 -- Tab navigation
 vim.keymap.set('n', '<C-j>', ':tabp<CR>')
