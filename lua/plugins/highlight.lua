10053067e9b3360e1626abf97ba09216502a41b2
Shane Carr <shane.carr@mmltd.co.za>
2025-08-28 08:00:04 +0200

    add context highlighting

---
+++ lua/plugins/highlight.lua
@@ -0,0 +1,9 @@
+return {
+    "LucasGualtieri/visual-highlight.nvim",
+    config = function()
+        require('visual-highlight').setup({
+            -- Custom configuration (optional)
+            highlight_color = "#3a3a3a",  -- Default highlight color
+        })
+    end
+}
