eda4c16cc03549b9119a08d6983f3ad5825087b4
Shane Carr <shanecarr1000@gmail.com>
2025-10-10 08:56:03 +0200

    Add some ease of life commandes
    - Run python from file
    - Open Nautilus here
    - Open code here
    - Copy to system clipboard
    - List pwd

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -62,3 +62,35 @@ vim.keymap.set("n", "<S-l>", "<C-w>l") -- Switch to window on the right
 vim.keymap.set("n", "<S-h>", "<C-w>h") -- Switch to window on the left
 vim.keymap.set("n", "<S-k>", "<C-w>k") -- Switch to window above
 vim.keymap.set("n", "<S-j>", "<C-w>j") -- Switch to window below
+
+-- Ease of Life
+vim.keymap.set("n", "<leader>pwd", ":pwd<Cr>") -- get current working directory
+
+-- Open Code here and close opened buffer
+vim.keymap.set("n", "<leader>cd", function()
+    vim.cmd("tabnew")
+    vim.cmd("terminal code .")
+    vim.cmd("q")
+end)
+-- Open file explorer here and close opened buffer
+vim.keymap.set("n", "<leader>fe", function()
+    vim.cmd("tabnew")
+    vim.cmd("terminal nautilus .")
+    vim.cmd("q")
+end)
+
+-- Better Copy Out
+vim.keymap.set("n", "<S-y>", function()
+    vim.cmd("call feedkeys('\"+y')")
+end, { noremap = true, silent = true })
+
+-- Python
+-- Run python file
+vim.keymap.set("n", "<leader>pr", function()
+    local filename = vim.fn.expand('%:p')
+    vim.cmd("sp")
+    vim.cmd("wincmd j")
+    vim.cmd("resize 15")
+    vim.cmd("terminal python3 " .. filename)
+end)
+
